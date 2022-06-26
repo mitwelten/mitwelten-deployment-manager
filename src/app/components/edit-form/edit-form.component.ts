dac0f8fa24f0c57b8125af972cb9d82f7a0e8308
Cedric Spindler <cedric.spindler@gmail.com>
2022-06-26 02:21:19 +0200

    Scaffold node editor

--- src/app/components/edit-form/edit-form.component.ts
+++ src/app/components/edit-form/edit-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
+import { ActivatedRoute } from '@angular/router';
+import { DataService, Node } from 'src/app/shared';
 
 @Component({
   selector: 'app-edit-form',
@@ -9,11 +10,20 @@ import { MatInputModule } from '@angular/material/input';
 })
 export class EditFormComponent implements OnInit {
 
+  nodes: Node[] | [] = [];
+
   name = new FormControl('');
 
-  constructor() { }
+  constructor(
+    private dataService: DataService,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    if ('id' in this.route.snapshot.params) {
+      const id = Number(this.route.snapshot.params['id']);
+    }
+    this.dataService.listNodes().subscribe(nodes => this.nodes = nodes);
   }
 
 }
