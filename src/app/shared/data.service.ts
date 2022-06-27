fbc97b0ce45deb5751ae00407c7f5959f8dfe2c9
Cedric Spindler <cedric.spindler@gmail.com>
2022-06-27 14:56:21 +0200

    Take api url from environment

--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -5,10 +5,14 @@ import { Observable, of } from 'rxjs';
 import { Deployment } from './deployment.type';
 import { Node } from './node.type';
 
+import { environment } from 'src/environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+
+  private apiUrl = environment.apiUrl;
 
   constructor(public readonly http: HttpClient) {}
 
@@ -27,63 +31,63 @@ export class DataService {
         from?: string,
         to?: string,
     ): Observable<Array<Node>> {
-        return this.http.get<Array<Node>>('http://localhost:8000/nodes', {});
+        return this.http.get<Array<Node>>(`${this.apiUrl}/nodes`, {});
     }
 
     public listDeployments(
         from?: string,
         to?: string,
     ): Observable<Array<Deployment>> {
-        return this.http.get<Array<Deployment>>('http://localhost:8000/deployments', {});
+        return this.http.get<Array<Deployment>>(`${this.apiUrl}/deployments`, {});
     }
 
     public getNodeById(id: number): Observable<Node> {
-      return this.http.get<Node>(`http://localhost:8000/node/${id}`, {});
+      return this.http.get<Node>(`${this.apiUrl}/node/${id}`, {});
     }
 
     public deleteNode(id: number): Observable<boolean> {
-      return this.http.delete<boolean>(`http://localhost:8000/node/${id}`);
+      return this.http.delete<boolean>(`${this.apiUrl}/node/${id}`);
     }
 
     public getDeploymentById(id: number): Observable<Deployment> {
-      return this.http.get<Deployment>(`http://localhost:8000/deployment/${id}`, {});
+      return this.http.get<Deployment>(`${this.apiUrl}/deployment/${id}`, {});
     }
 
     public putDeployment(deployment: any) {
-      return this.http.put(`http://localhost:8000/deployments`, deployment, { });
+      return this.http.put(`${this.apiUrl}/deployments`, deployment, { });
     }
 
     public deleteDeployment(id: number): Observable<boolean> {
-      return this.http.delete<boolean>(`http://localhost:8000/deployment/${id}`);
+      return this.http.delete<boolean>(`${this.apiUrl}/deployment/${id}`);
     }
 
 
     public putNode(node: Partial<Node>): Observable<number> {
-      return this.http.put<number>(`http://localhost:8000/nodes`, node, { });
+      return this.http.put<number>(`${this.apiUrl}/nodes`, node, { });
     }
 
     public isDeploymentOverlapping(deployment: any) {
-      return this.http.put<boolean>(`http://localhost:8000/validate/deployment`, deployment, {});
+      return this.http.put<boolean>(`${this.apiUrl}/validate/deployment`, deployment, {});
     }
 
     public isNodeUnique(node: Partial<Node>) {
-      return this.http.put<boolean>(`http://localhost:8000/validate/node`, node, {});
+      return this.http.put<boolean>(`${this.apiUrl}/validate/node`, node, {});
     }
 
     public getNodeTypeOptions(searchTerm: string) {
-      return this.http.get<string[]>(`http://localhost:8000/node/type_options/${searchTerm}`);
+      return this.http.get<string[]>(`${this.apiUrl}/node/type_options/${searchTerm}`);
     }
 
     public getNodePlatformOptions(searchTerm: string) {
-      return this.http.get<string[]>(`http://localhost:8000/node/platform_options/${searchTerm}`);
+      return this.http.get<string[]>(`${this.apiUrl}/node/platform_options/${searchTerm}`);
     }
 
     public getNodeConnectivityOptions(searchTerm: string) {
-      return this.http.get<string[]>(`http://localhost:8000/node/connectivity_options/${searchTerm}`);
+      return this.http.get<string[]>(`${this.apiUrl}/node/connectivity_options/${searchTerm}`);
     }
 
     public getNodePowerOptions(searchTerm: string) {
-      return this.http.get<string[]>(`http://localhost:8000/node/power_options/${searchTerm}`);
+      return this.http.get<string[]>(`${this.apiUrl}/node/power_options/${searchTerm}`);
     }
 
 }
