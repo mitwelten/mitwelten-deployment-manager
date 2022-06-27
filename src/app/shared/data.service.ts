b8520ecd842e0a58b0fe159e55e5e696e763a414
Cedric Spindler <cedric.spindler@gmail.com>
2022-06-27 18:27:50 +0200

    Fix code style / indentation

--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import { Deployment } from './deployment.type';
 import { Node } from './node.type';
@@ -16,78 +16,60 @@ export class DataService {
 
   constructor(public readonly http: HttpClient) {}
 
-    /**
-     * List Nodes
-     * List all nodes
-     *
-     * This is a temporary, hacky, but working implementation. It becomes obvious
-     * that the database schema requires an abstraction of `deployment`.
-     * @param from
-     * @param to
-     * @returns Node Successful Response
-     * @throws ApiError
-     */
-    public listNodes(
-        from?: string,
-        to?: string,
-    ): Observable<Array<Node>> {
-        return this.http.get<Array<Node>>(`${this.apiUrl}/nodes`, {});
-    }
+  public listNodes(): Observable<Array<Node>> {
+    return this.http.get<Array<Node>>(`${this.apiUrl}/nodes`);
+  }
 
-    public listDeployments(
-        from?: string,
-        to?: string,
-    ): Observable<Array<Deployment>> {
-        return this.http.get<Array<Deployment>>(`${this.apiUrl}/deployments`, {});
-    }
+  public listDeployments(): Observable<Array<Deployment>> {
+    return this.http.get<Array<Deployment>>(`${this.apiUrl}/deployments`);
+  }
 
-    public getNodeById(id: number): Observable<Node> {
-      return this.http.get<Node>(`${this.apiUrl}/node/${id}`, {});
-    }
+  public getNodeById(id: number): Observable<Node> {
+    return this.http.get<Node>(`${this.apiUrl}/node/${id}`);
+  }
 
-    public deleteNode(id: number): Observable<boolean> {
-      return this.http.delete<boolean>(`${this.apiUrl}/node/${id}`);
-    }
+  public deleteNode(id: number): Observable<boolean> {
+    return this.http.delete<boolean>(`${this.apiUrl}/node/${id}`);
+  }
 
-    public getDeploymentById(id: number): Observable<Deployment> {
-      return this.http.get<Deployment>(`${this.apiUrl}/deployment/${id}`, {});
-    }
+  public getDeploymentById(id: number): Observable<Deployment> {
+    return this.http.get<Deployment>(`${this.apiUrl}/deployment/${id}`);
+  }
 
-    public putDeployment(deployment: any) {
-      return this.http.put(`${this.apiUrl}/deployments`, deployment, { });
-    }
+  public putDeployment(deployment: any) {
+    return this.http.put(`${this.apiUrl}/deployments`, deployment);
+  }
 
-    public deleteDeployment(id: number): Observable<boolean> {
-      return this.http.delete<boolean>(`${this.apiUrl}/deployment/${id}`);
-    }
+  public deleteDeployment(id: number): Observable<boolean> {
+    return this.http.delete<boolean>(`${this.apiUrl}/deployment/${id}`);
+  }
 
+  public putNode(node: Partial<Node>): Observable<number> {
+    return this.http.put<number>(`${this.apiUrl}/nodes`, node);
+  }
 
-    public putNode(node: Partial<Node>): Observable<number> {
-      return this.http.put<number>(`${this.apiUrl}/nodes`, node, { });
-    }
+  public isDeploymentOverlapping(deployment: any) {
+    return this.http.put<boolean>(`${this.apiUrl}/validate/deployment`, deployment);
+  }
 
-    public isDeploymentOverlapping(deployment: any) {
-      return this.http.put<boolean>(`${this.apiUrl}/validate/deployment`, deployment, {});
-    }
+  public isNodeUnique(node: Partial<Node>) {
+    return this.http.put<boolean>(`${this.apiUrl}/validate/node`, node);
+  }
 
-    public isNodeUnique(node: Partial<Node>) {
-      return this.http.put<boolean>(`${this.apiUrl}/validate/node`, node, {});
-    }
+  public getNodeTypeOptions(searchTerm: string) {
+    return this.http.get<string[]>(`${this.apiUrl}/node/type_options/${searchTerm}`);
+  }
 
-    public getNodeTypeOptions(searchTerm: string) {
-      return this.http.get<string[]>(`${this.apiUrl}/node/type_options/${searchTerm}`);
-    }
+  public getNodePlatformOptions(searchTerm: string) {
+    return this.http.get<string[]>(`${this.apiUrl}/node/platform_options/${searchTerm}`);
+  }
 
-    public getNodePlatformOptions(searchTerm: string) {
-      return this.http.get<string[]>(`${this.apiUrl}/node/platform_options/${searchTerm}`);
-    }
+  public getNodeConnectivityOptions(searchTerm: string) {
+    return this.http.get<string[]>(`${this.apiUrl}/node/connectivity_options/${searchTerm}`);
+  }
 
-    public getNodeConnectivityOptions(searchTerm: string) {
-      return this.http.get<string[]>(`${this.apiUrl}/node/connectivity_options/${searchTerm}`);
-    }
-
-    public getNodePowerOptions(searchTerm: string) {
-      return this.http.get<string[]>(`${this.apiUrl}/node/power_options/${searchTerm}`);
-    }
+  public getNodePowerOptions(searchTerm: string) {
+    return this.http.get<string[]>(`${this.apiUrl}/node/power_options/${searchTerm}`);
+  }
 
 }
