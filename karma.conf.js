15ab1f3bce4b07138ee777f395dd1eff2e28d3bd
Cedric Spindler <cedric.spindler@gmail.com>
2022-09-04 20:16:54 +0200

    Consistent naming

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/node-entry'),
+      dir: require('path').join(__dirname, './coverage/deploy-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
