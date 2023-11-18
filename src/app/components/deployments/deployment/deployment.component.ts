ce4fca1944baa24601e2e7424b0a3b57f11028c9
Cedric Spindler <cedric.spindler@gmail.com>
2023-11-18 19:00:33 +0100

    Set filter by selected tag in deployment view

--- src/app/components/deployments/deployment/deployment.component.ts
+++ src/app/components/deployments/deployment/deployment.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component, Inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormGroup } from '@angular/forms';
 
 import { MatButtonModule } from '@angular/material/button';
-import { MatChipsModule } from '@angular/material/chips';
+import { MatChipSelectionChange, MatChipsModule } from '@angular/material/chips';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 
 import { Deployment } from 'src/app/shared';
 import { MapComponent } from '../../map.component';
+import { FilterStoreService } from 'src/app/services';
 
 @Component({
   selector: 'app-deployment',
@@ -25,13 +27,16 @@ import { MapComponent } from '../../map.component';
 export class DeploymentComponent {
 
   deployment: Deployment;
+  filter: FormGroup
 
   constructor (
     public dialogRef: MatDialogRef<DeploymentComponent>,
+    public filterService: FilterStoreService,
     private router: Router,
     @Inject(MAT_DIALOG_DATA) deployment: Deployment,
     ) {
     this.deployment = deployment;
+    this.filter = filterService.deploymentsFilter;
   }
 
   edit(): void {
@@ -40,4 +45,19 @@ export class DeploymentComponent {
     });
     this.dialogRef.close();
   }
+
+  checkTagSelected(tag_id?: number): boolean {
+    return !!this.filterService.deploymentsFilter.controls.tags.value?.
+      filter(t => t === tag_id).length;
+  }
+
+  selectionChange(e: MatChipSelectionChange) {
+    if (e.isUserInput) {
+      const control = this.filterService.deploymentsFilter.controls.tags;
+      const tags = control.value ?? [];
+      if (e.selected) control.setValue([...tags, <number>e.source.value]);
+      else control.setValue(tags.filter(t => t !== e.source.value));
+    }
+  }
+
 }
